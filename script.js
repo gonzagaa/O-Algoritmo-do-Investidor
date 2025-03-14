41cf4c94ff004ffa8f3050cbe31f880ce5e623bb
gonzxgx <iigust4vo@gmail.com>
2025-03-14 17:37:59 -0300

    update

--- script.js
+++ script.js
@@ -25,17 +25,63 @@ if (larguraDaTela < 800) {
             translate: ["100%", 0, 0],
           },
         },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
       });
 } else {
     var swiper3 = new Swiper(".mySwiper3", {
-        slidesPerView: 4,
+        slidesPerView: 3,
         spaceBetween: 30,
         loop: true,
         grabCursor: true,
-        freeMode: true,
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
         },
       });
 }
+
+// Seleciona os novos botões para abrir o modal
+const openModalButtons = document.querySelectorAll('.openModalForm');
+const modalOverlay = document.getElementById('modalOverlay');
+const closeModalButton = document.getElementById('closeModal');
+
+// Variável para armazenar a origem do modal (WhatsApp ou Obrigado)
+let redirectType = null;
+
+// Função para abrir o modal e definir o tipo de redirecionamento
+function openModal(event) {
+    modalOverlay.classList.add('active');
+
+    // Verifica qual botão foi clicado e define o tipo de redirecionamento
+    if (event.target.closest('.btn-whatsapp-pulse')) {
+        redirectType = 'whatsapp';
+    } else {
+        redirectType = 'obrigado';
+    }
+}
+
+// Função para fechar o modal
+function closeModal() {
+    modalOverlay.classList.remove('active');
+    redirectType = null; // Reseta a variável
+}
+
+// Adiciona evento para abrir o modal nos novos botões
+openModalButtons.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+
+// Fecha o modal ao clicar no botão de fechar
+if (closeModalButton) {
+    closeModalButton.addEventListener('click', closeModal);
+}
+
+// Fecha o modal ao clicar na overlay
+modalOverlay.addEventListener('click', (event) => {
+    if (event.target === modalOverlay) {
+        closeModal();
+    }
+});
