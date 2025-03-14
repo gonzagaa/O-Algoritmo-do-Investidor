d3c739cccebc7c15005a772be4dae03c79c14473
gonzagaa <iigust4vo@gmail.com>
2025-03-14 00:48:46 -0300

    update

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ AOS.init(
   }
 );
 
+const larguraDaTela = window.innerWidth
+  
+if (larguraDaTela < 800) {
+    var swiper3 = new Swiper(".mySwiper3", {
+        grabCursor: true,
+        effect: "creative",
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+          },
+        creativeEffect: {
+          prev: {
+            shadow: true,
+            translate: ["-20%", 0, -1],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        },
+      });
+} else {
+    var swiper3 = new Swiper(".mySwiper3", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        grabCursor: true,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
